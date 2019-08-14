38e1291583fcd5528f090054dcb7d3cdff6c1cea
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-08-14 09:14:53 +0900

    Add files via upload

---
+++ Data_Type.java
@@ -0,0 +1,24 @@
+
+public class Data_Type {
+
+	public static void main(String[] args) {
+		
+		// 10진수를 8진수, 16진수로 바꾸기
+		// int a = 200;
+		
+		// System.out.println("10 진수 :" + a);
+        // System.out.format("8 진수 : %o\n", a);
+        // System.out.format("16 진수 : %x", a);
+        
+		
+		// String의 substring 함수 할용 해보기
+		
+		String name = "John Doe";
+		System.out.println(name);
+	    System.out.println(name.substring(0, 1));
+	    System.out.println(name.substring(3, 6));
+	    System.out.println(name.substring(5, 8));
+	    System.out.println(name.substring(0, 4));
+	}
+
+}
