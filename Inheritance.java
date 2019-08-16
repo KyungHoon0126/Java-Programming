f3fb57f895bb06fcf684d2eb52523ae021b09d70
Kyung_Hoon <48943501+KyungHoon0126@users.noreply.github.com>
2019-08-16 09:51:28 +0900

    Add files via upload

---
+++ Inheritance.java
@@ -0,0 +1,15 @@
+
+public class Inheritance {
+
+	public static void main(String[] args) {
+		
+		Inheritance_Student student1 = new Inheritance_Student("홍길동", 20, 175, 70, "20190814", 1, 4.5);
+		Inheritance_Student student2 = new Inheritance_Student("이순신", 30, 170, 80, "20190815", 4, 3.0);
+
+		student1.show();
+		student2.show();
+		
+	}
+
+}
+ 
