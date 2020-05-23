34006882ef9472cd78b6cb1adf2590cff65f0b22
김경훈 <48943501+KyungHoon0126@users.noreply.github.com>
2020-05-23 17:49:23 +0900

    [ADD] : Interface_Study1

---
+++ Countable.java
@@ -0,0 +1,6 @@
+package kr.hs.dgsw.c1.d0512;
+
+public interface Countable {
+
+	public int count();
+}
---
+++ Printable.java
@@ -0,0 +1,7 @@
+package kr.hs.dgsw.c1.d0512;
+
+public interface Printable {
+
+	public void print();
+	
+}
